9cc6f116010ae6995146262e7e854f7d99d5c15e
Pamir Erdem <pamir.erdem@acedemand.com>
2020-01-28 00:58:45 +0300

    #1

--- src/main/java/com/microsoft/dao/AnomalyDetectorDao.java
+++ src/main/java/com/microsoft/dao/AnomalyDetectorDao.java
@@ -14,14 +14,16 @@ public class AnomalyDetectorDao {
 
 
     private String subscriptionKey;
+    private String adServer;
 
-    public AnomalyDetectorDao(String subscriptionKey){
+    public AnomalyDetectorDao(String subscriptionKey,String adServer){
         this.subscriptionKey = subscriptionKey;
+        this.adServer = adServer;
     }
 
     public BatchResponse find(BatchRequest batchRequest) {
         Client client = ClientBuilder.newClient();
-        WebTarget webTarget = client.target("http://localhost:5000");
+        WebTarget webTarget = client.target(this.adServer);
         WebTarget employeeWebTarget = webTarget.path("/anomalydetector/v1.0/timeseries/entire/detect");
         Invocation.Builder invocationBuilder = employeeWebTarget.request(MediaType.APPLICATION_JSON);
         invocationBuilder.header("Ocp-Apim-Subscription-Key",this.subscriptionKey);
--- src/main/java/com/microsoft/logstashplugins/MsAdOutputplugin.java
+++ src/main/java/com/microsoft/logstashplugins/MsAdOutputplugin.java
@@ -26,12 +26,14 @@ public class MsAdOutputplugin implements Output {
     public static final PluginConfigSpec<String> TIMESTAMP_COLUMN = PluginConfigSpec.stringSetting("ts_column", "ts");
     public static final PluginConfigSpec<String> TIMESERIES_GRANULARITY = PluginConfigSpec.stringSetting("ts_granularity", "");
     public static final PluginConfigSpec<String> TIMESERIES_VALUE_COLUMN = PluginConfigSpec.stringSetting("ts_value_column", "TotalOperationDuration");
+    public static final PluginConfigSpec<String> AD_SERVER = PluginConfigSpec.stringSetting("ad_server", "http://localhost:5000");
 
     private final String id;
     private final String granularity;
     private final String api_key;
     private final String ts_value_column;
     private final String timestamp_column;
+    private final String ad_server;
 
     private PrintStream printer;
     private final CountDownLatch done = new CountDownLatch(1);
@@ -53,6 +55,7 @@ public class MsAdOutputplugin implements Output {
         this.timestamp_column = config.get(TIMESTAMP_COLUMN);
         this.granularity = config.get(TIMESERIES_GRANULARITY);
         this.ts_value_column = config.get(TIMESERIES_VALUE_COLUMN);
+        this.ad_server = config.get(AD_SERVER);
         
         this.batchRequest = new BatchRequest(new ArrayList<>(),this.granularity);
 
@@ -90,7 +93,7 @@ public class MsAdOutputplugin implements Output {
 
     @Override
     public void stop() {
-        AnomalyDetectorDao dao = new AnomalyDetectorDao(this.api_key);
+        AnomalyDetectorDao dao = new AnomalyDetectorDao(this.api_key,this.ad_server);
         Collections.sort((ArrayList<Point>)batchRequest.getSeries());
         ((ArrayList<Point>) batchRequest.getSeries()).remove(batchRequest.getSeries().size() / 2);
         printer.println(new Gson().toJson(batchRequest));
@@ -115,6 +118,7 @@ public class MsAdOutputplugin implements Output {
         ((ArrayList<PluginConfigSpec<?>>) configSpecs).add(TIMESTAMP_COLUMN);
         ((ArrayList<PluginConfigSpec<?>>) configSpecs).add(TIMESERIES_VALUE_COLUMN);
         ((ArrayList<PluginConfigSpec<?>>) configSpecs).add(TIMESERIES_GRANULARITY);
+        ((ArrayList<PluginConfigSpec<?>>) configSpecs).add(AD_SERVER);
 
         return configSpecs;
     }

cc076f53f07ce0a156a1aafa650d3717a6f2dc9a
Pamir Erdem <pamir.erdem@acedemand.com>
2020-02-08 20:10:29 +0300

    format code

--- src/main/java/com/microsoft/dao/AnomalyDetectorDao.java
+++ src/main/java/com/microsoft/dao/AnomalyDetectorDao.java
@@ -1,27 +1,22 @@
 
 package com.microsoft.dao;
 
-
 import com.google.gson.Gson;
 import com.microsoft.pojo.BatchRequest;
 import com.microsoft.pojo.BatchResponse;
-
 import java.io.PrintStream;
-
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-
 public class AnomalyDetectorDao {
-
 
     private String subscriptionKey;
     private String adServer;
     private PrintStream printer;
 
-    public AnomalyDetectorDao(String subscriptionKey,String adServer, PrintStream printer){
+    public AnomalyDetectorDao(String subscriptionKey, String adServer, PrintStream printer) {
         this.subscriptionKey = subscriptionKey;
         this.adServer = adServer;
         this.printer = printer;
@@ -32,20 +27,15 @@ public class AnomalyDetectorDao {
         WebTarget webTarget = client.target(this.adServer);
         WebTarget anomalyWebTarget = webTarget.path("/anomalydetector/v1.0/timeseries/entire/detect");
         Invocation.Builder invocationBuilder = anomalyWebTarget.request(MediaType.APPLICATION_JSON);
-        invocationBuilder.header("Ocp-Apim-Subscription-Key",this.subscriptionKey);
-        Response response = invocationBuilder.post(Entity.entity(batchRequest,MediaType.APPLICATION_JSON_TYPE));
+        invocationBuilder.header("Ocp-Apim-Subscription-Key", this.subscriptionKey);
+        Response response = invocationBuilder.post(Entity.entity(batchRequest, MediaType.APPLICATION_JSON_TYPE));
 
         String rawResponse = response.readEntity(String.class);
-        if(response.getStatus() == 200){
+        if (response.getStatus() == 200) {
             return new Gson().fromJson(rawResponse, BatchResponse.class);
         }
         printer.print(rawResponse);
         throw new ProcessingException(rawResponse);
-        
-            
-        
-        
-
     }
 
 }
